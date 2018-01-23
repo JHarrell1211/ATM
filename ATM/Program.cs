473feabbe4fedd8427b93764b3210b1903c72f14
JHarrell1211 <julian1211@gmail.com>
2018-01-23 15:33:55 -0500

    Working ATM

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -10,6 +10,71 @@ namespace ATM
     {
         static void Main(string[] args)
         {
+            double accountBal = 4189.07;
+            int transaction = 0;
+            double withdrawl = 0;
+            double deposit = 0;
+
+            do
+            {
+                Console.WriteLine("Enter The Number of Your Desired Transaction Type:");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdrawl");
+                Console.WriteLine("3. Account Balance");
+                Console.WriteLine("4. Quit");
+                transaction = int.Parse(Console.ReadLine());
+                
+                if ((transaction < 1) || (transaction > 4))
+                {
+                    Console.WriteLine("Invald Entry!");
+                }
+
+                if (transaction == 1)
+                {
+                    Console.WriteLine("Enter Deposit Amount");
+                    deposit = int.Parse(Console.ReadLine());
+
+                    while (deposit < 0)
+                    {
+                        Console.WriteLine("Error!! Enter Deposit Amount: ");
+                        deposit = int.Parse(Console.ReadLine());
+                    } 
+
+                        accountBal = (deposit + accountBal);
+                        Console.WriteLine(accountBal);
+                        Console.WriteLine("Your New Balance is: " + accountBal + "\n");    
+                }
+
+                if (transaction == 2)
+                {
+                        Console.WriteLine("Enter Withdrawl Amount: ");
+                        withdrawl = int.Parse(Console.ReadLine());
+
+                    while (withdrawl > accountBal)
+                    {
+                        Console.WriteLine("Insufficient Funds: Your Account Balance is: " + accountBal);
+                        Console.WriteLine("Enter Withdrawl Amount: ");
+                        withdrawl = int.Parse(Console.ReadLine());
+                    }
+
+                        accountBal = accountBal - withdrawl;
+                        Console.WriteLine("Your Account Balance is " + accountBal + "\n" );
+                    
+                }
+                if (transaction == 3)
+                {
+                    Console.WriteLine("Your Account Balance is: " + accountBal + "\n");
+                }
+                if (transaction == 4)
+                {
+                    Console.WriteLine("Good Bye!");
+                }
+            }
+            while (transaction != 4);
+                  
+                
+            
+
         }
     }
 }
